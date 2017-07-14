62b988dfae12fbb737bf8c102bdce62201d85614
Arianna Chau <ariannapikachau@gmail.com>
2017-07-14 15:25:25 -0700

    Start styling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  // styleUrls: ['./about.component.scss'],
+  styles: [`
+    .body {
+      background-color: blue;
+    }
+  `],
 })
 export class AboutComponent implements OnInit {
 
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,7 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  // styleUrls: ['./welcome.component.scss']
+  styles: [`
+    body {
+      height: 100vh;
+      width: 100vw;
+      margin: 0;
+      padding: 0;
+      background-image: url(/assets/images/hero.jpg);
+      background-size: cover;
+      background-repeat: no-repeat;
+      }
+    #hero-title {
+      height: 100vh;
+      width: 100vw;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 45px;
+      color: white;
+      text-shadow: 2px 2px 2px black;
+    }
+  `],
 })
 export class WelcomeComponent implements OnInit {
 
