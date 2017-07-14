2161412a6c86cd66cf120bae85f1bc7d3daf4bb6
Arianna Chau <ariannapikachau@gmail.com>
2017-07-14 10:08:07 -0700

    Add sample members.

---
+++ src/app/member.model.ts
@@ -0,0 +1,6 @@
+export class Member {
+  constructor (
+    public name: string,
+    public age: number,
+    public faveLocation: string) { }
+}
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-members',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent implements OnInit {
+  members: Member[] = [
+
+   new Member("Arianna Chau", 22,
+       "Trillium Lake"),
+
+   new Member("Steven Galvin", 23,
+       "Mirror Lake"),
+
+   new Member("Taylor Almond", 22,
+       "Mystic Falls"),
+
+   new Member("McKenzie Engelgau", 22,
+       "Angels Rest"),
+
+   new Member("Carrie Lewis", 46,
+       "Little ZigZag Falls"),
+ ];
 
   constructor() { }
 
